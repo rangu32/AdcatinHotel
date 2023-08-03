da0bd604742e72fb7834d9e85d0bba62cd982791
rangu32 <rangu32@gmail.com>
2023-08-03 18:12:16 +0530

    added homepage

--- AdactinHotel/src/main/java/com/pages/HomePage.java
+++ AdactinHotel/src/main/java/com/pages/HomePage.java
@@ -1,9 +1,14 @@
 package com.pages;
 
 public class HomePage {
+    public static void main(String[] args) {
 
-	private void ProjectCreate() {
-		System.out.println("Sucessfully created project");
-	}
+
+
+    }
+    private void ProjectCreate() {
+
+        System.out.println("Sucessfully created project");
+    }
 
 }
